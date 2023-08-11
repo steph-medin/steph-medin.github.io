c1b58143f2201fb4307e4d5aab2d1b878ffe4bcb
Stephan Medina <70229489+steph-medin@users.noreply.github.com>
2023-08-11 14:19:47 -0600

    Update 33: Layout changes

--- JS/project.js
+++ JS/project.js
@@ -2,3 +2,9 @@ let btnBack = document.querySelector('button');
 btnBack.addEventListener('click', () => {
     window.history.back();
 });
+
+const shrinkableBox = document.getElementById("shrinkable-box");
+
+shrinkableBox.addEventListener("click", () => {
+  shrinkableBox.style.transform = "scale(0.8)";
+});
